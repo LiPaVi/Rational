9e4af6961453b13209b316687c70531a9a3a0a1c
lpalacheva <LPalacheva@sportmaster.ru>
2019-10-30 00:29:27 +0300

    added test for methods: lessOrEqual, plus, multiply, minus, devide. 100% test coverage.

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -5,8 +5,10 @@ public class Tests {
     @Test
     public void testStandardConstructor() {
         Rational standard = new Rational();
-        assertEquals("Standard constructor returns wrong numerator", 0, standard.getNumerator());
-        assertEquals("Standard constructor returns wrong denominator", 1, standard.getDenominator());
+        assertEquals("Standard constructor returns wrong numerator",
+                0, standard.getNumerator());
+        assertEquals("Standard constructor returns wrong denominator",
+                1, standard.getDenominator());
     }
 
     @Test
@@ -15,8 +17,10 @@ public class Tests {
         int denominator = 2;
 
         Rational rational = new Rational(numerator,denominator);
-        assertEquals("getNumerator returns wrong numerator", numerator, rational.getNumerator());
-        assertEquals("getDenominator returns wrong denominator", denominator, rational.getDenominator());
+        assertEquals("getNumerator returns wrong numerator",
+                numerator, rational.getNumerator());
+        assertEquals("getDenominator returns wrong denominator",
+                denominator, rational.getDenominator());
     }
 
     @Test
@@ -27,8 +31,17 @@ public class Tests {
         Rational rational = new Rational();
         rational.setNumerator(numerator);
         rational.setDenominator(denominator);
-        assertEquals("setNumerator sets wrong numerator", numerator, rational.getNumerator());
-        assertEquals("setDenominator sets wrong denominator", denominator, rational.getDenominator());
+        assertEquals("setNumerator sets wrong numerator",
+                numerator, rational.getNumerator());
+        assertEquals("setDenominator sets wrong denominator",
+                denominator, rational.getDenominator());
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testWrongRationalConstruction(){
+        Rational wrongRational = new Rational(1, 0);
+        fail("testWrongRationalConstruction() failed. " +
+                "\n There's no arithmeticException when denominator is 0. ");
     }
 
     @Test
@@ -37,8 +50,9 @@ public class Tests {
         int denominator = 2;
         String expectedResult = "1/2";
 
-        String positive = new Rational(numerator,denominator).toString();
-        assertEquals("toString returns wrong string value for positive rational", expectedResult, positive);
+        String rational = new Rational(numerator,denominator).toString();
+        assertEquals("toString returns wrong string value for positive rational",
+                expectedResult, rational);
     }
 
     @Test
@@ -47,8 +61,9 @@ public class Tests {
         int denominator = 2;
         String expectedResult = "-1/2";
 
-        String positive = new Rational(numerator,denominator).toString();
-        assertEquals("toString returns wrong string value for negative numerator", expectedResult, positive);
+        String rational = new Rational(numerator,denominator).toString();
+        assertEquals("toString returns wrong string value for negative numerator",
+                expectedResult, rational);
     }
 
     @Test
@@ -57,8 +72,9 @@ public class Tests {
         int denominator = -2;
         String expectedResult = "-1/2";
 
-        String positive = new Rational(numerator,denominator).toString();
-        assertEquals("toString returns wrong string value for negative denominator",  expectedResult, positive);
+        String rational = new Rational(numerator,denominator).toString();
+        assertEquals("toString returns wrong string value for negative denominator",
+                expectedResult, rational);
     }
 
     @Test
@@ -67,8 +83,31 @@ public class Tests {
         int denominator = -2;
         String expectedResult = "1/2";
 
-        String positive = new Rational(numerator,denominator).toString();
-        assertEquals("toString returns wrong string value for negative numerator and denominator",  expectedResult, positive);
+        String rational = new Rational(numerator,denominator).toString();
+        assertEquals("toString returns wrong string value for negative numerator and denominator",
+                expectedResult, rational);
+    }
+
+    @Test
+    public void testReduction() {
+        int numerator = 100;
+        int denominator = 15;
+        String expectedResult = "20/3";
+
+        String rational = new Rational(numerator,denominator).toString();
+        assertEquals("toString returns wrong string value when reduce",
+                expectedResult, rational);
+    }
+
+    @Test
+    public void testReductionWhithZeroNumerator() {
+        int numerator = 0;
+        int denominator = -15;
+        String expectedResult = "0/1";
+
+        String rational = new Rational(numerator,denominator).toString();
+        assertEquals("toString returns wrong string value when numerator = 0. ",
+                expectedResult, rational);
     }
 
     @Test
@@ -88,6 +127,14 @@ public class Tests {
     }
 
     @Test
+    public void testNotEqualsObjects(){
+        Rational rational = new Rational();
+        String string = "0/1";
+
+        assertFalse("Not equals objects", rational.equals(string));
+    }
+
+    @Test
     public void testIsLess(){
         Rational firstRational = new Rational(1, 2);
         Rational secondRational = new Rational(2, 1);
@@ -99,8 +146,8 @@ public class Tests {
 
     @Test
     public void testIsMoreNotLess(){
-        Rational firstRational = new Rational(3, 2);
-        Rational secondRational = new Rational(1, 2);
+        Rational firstRational = new Rational(3, 1);
+        Rational secondRational = new Rational(3, 2);
 
         assertFalse("Method less returns wrong boolean value for: "
                         + firstRational + " > " + secondRational,
@@ -120,7 +167,7 @@ public class Tests {
     @Test
     public void testIsLessForNegativeRational(){
         Rational firstRational = new Rational(1, -2);
-        Rational secondRational = new Rational(1, 3);
+        Rational secondRational = new Rational(2, 4);
 
         assertTrue("Method less returns wrong boolean value for: "
                         + firstRational + " < " + secondRational,
@@ -137,12 +184,83 @@ public class Tests {
                 firstRational.less(secondRational));
     }
 
+    @Test
+    public void testLessForLessOrEqual(){
+        Rational firstRational = new Rational(0, 1);
+        Rational secondRational = new Rational(1, 3);
+
+        assertTrue("Method lessOrEqual returns wrong boolean value for: "
+                        + firstRational + " <= " + secondRational,
+                firstRational.lessOrEqual(secondRational));
+    }
+
+    @Test
+    public void testEqualForLessOrEqual(){
+        Rational firstRational = new Rational(0, 1);
+        Rational secondRational = new Rational(0, 3);
+
+        assertTrue("Method lessOrEqual returns wrong boolean value for: "
+                        + firstRational + " <= " + secondRational,
+                firstRational.lessOrEqual(secondRational));
+    }
+
+    @Test
+    public void testMoreForLessOrEqual(){
+        Rational firstRational = new Rational(10, 1);
+        Rational secondRational = new Rational(1, 3);
+
+        assertFalse("Method lessOrEqual returns wrong boolean value for: "
+                        + firstRational + " > " + secondRational,
+                firstRational.less(secondRational));
+    }
+
+    @Test
+    public void testAdditionRational(){
+        Rational firstRational = new Rational(10, 1);
+        Rational secondRational = new Rational(1, 3);
+        Rational expectedSum = new Rational(31, 3);
+
+        assertEquals("The addition of two Rationals is wrong",
+                expectedSum,firstRational.plus(secondRational));
+    }
+
+    @Test
+    public void testMultiplicationRational(){
+        Rational firstRational = new Rational(10, 1);
+        Rational secondRational = new Rational(1, 3);
+        Rational expectedSum = new Rational(10, 3);
+
+        assertEquals("The multiplication of two Rationals is wrong",
+                expectedSum,firstRational.multiply(secondRational));
+    }
+
+    @Test
+    public void testSubtractionRational(){
+        Rational firstRational = new Rational(10, 1);
+        Rational secondRational = new Rational(1, 3);
+        Rational expectedSum = new Rational(29, 3);
+
+        assertEquals("The subtraction of two Rationals is wrong",
+                expectedSum,firstRational.minus(secondRational));
+    }
+
+    @Test
+    public void testDivisionRational(){
+        Rational firstRational = new Rational(10, 1);
+        Rational secondRational = new Rational(1, 3);
+        Rational expectedSum = new Rational(30, 1);
+
+        assertEquals("The division of two Rationals is wrong",
+                expectedSum,firstRational.divide(secondRational));
+    }
+
     @Test(expected = ArithmeticException.class)
     public void testDevisionByZero(){
         Rational firstRational = new Rational(1, 2);
         Rational secondRational = new Rational(0, 1);
 
         firstRational.divide(secondRational);
+        fail("testDevisionByZero() failed. " +
+                "\n There's no arithmeticException when devision by zero.");
     }
-
 }
